e8e40b7d93ecee54a3e9f5265bbfd7341b8cc417
Pantha-Adarsha <panthaadarsha0@gmail.com>
2024-07-19 13:29:39 +0545

    chnages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 app.get("/", (req, res) => {
     res.render("login");
